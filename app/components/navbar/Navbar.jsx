aaf8111bc17a74ed1b3022a03175b544e073a0c7
muhammad subhan <muhammedsubhan692@gmail.com>
2024-10-02 04:08:35 +0500

    Navbar

---
+++ app/components/navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useState } from "react";
+import logo from "@/images/Logo.png";
+import arrow_down from "@/images/arrow-down.png";
+import Image from "next/image";
+import Hamburger from "hamburger-react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="flex">
+        <div className="px-12 lg:px-0 lg:pl-8 ">
+          <p className="border-r-[2px] border-[#ABB2BF] h-[380px] rounded-full	"></p>
+        </div>
+        <div className="flex items-center justify-between px-40  h-24 w-full xl:px-12">
+          <div className="flex items-center gap-3">
+            <Image src={logo} height={50} width={40} alt="logo" quality={100} />
+            <h1 className="font-medium text-2xl text-white">Subhan</h1>
+          </div>
+
+          <div className="hidden md:flex">
+            <Hamburger toggled={isOpen} toggle={setIsOpen} color="#D9D9D9" />
+          </div>
+
+          <div className="flex items-center justify-between md:hidden">
+            <ul className="flex items-center gap-8 lg:gap-6">
+              <li className="text-white text-xl font-medium flex gap-1">
+                <span className="text-[#C778DD]">#</span>home
+              </li>
+              <li className="text-white text-xl font-medium flex gap-1">
+                <span className="text-[#C778DD]">#</span>works
+              </li>
+              <li className="text-white text-xl font-medium flex gap-1">
+                <span className="text-[#C778DD]">#</span>about-me
+              </li>
+              <li className="text-white text-xl font-medium flex gap-1">
+                <span className="text-[#C778DD]">#</span>contacts
+              </li>
+              <li>
+                <div className="flex items-center gap-1 ml-4 lg:ml-0">
+                  <p className="text-xl font-medium text-[#ABB2BF]">EN</p>
+                  <Image
+                    src={arrow_down}
+                    width={14}
+                    height={14}
+                    alt="arrow-icon"
+                  />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,23 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1025px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "768px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      xs: { max: "475px" },
     },
   },
   plugins: [],
